4d4d2cf43c8f2d3da49bb52d6f8152ad88a32e6d
KivaJing <jing.zhan.3826@student.uu.se>
2024-02-02 11:23:12 +0100

    add inline static to constant value

--- runner/include/ground.h
+++ runner/include/ground.h
@@ -4,10 +4,10 @@
 
 class Ground
 {
-	sf::FloatRect floor = { 0.0f, 550.0f, 1280.0f, 15.0f };	
-	sf::FloatRect barrierSize = { 1200.0f, 450.0f, 50.0f, 100.0f };
-	sf::Color barrier_color = { 200,200,200 };
-	sf::Vector2f m_Speed = { -400.0f, 0.0f };
+	inline static sf::FloatRect floor = { 0.0f, 550.0f, 1280.0f, 15.0f };	
+	inline static sf::FloatRect barrierSize = { 1200.0f, 450.0f, 50.0f, 100.0f };
+	inline static sf::Color barrier_color = { 200,200,200 };
+	inline static sf::Vector2f m_Speed = { -400.0f, 0.0f };
 	std::vector <RectEntity> barriers;
 	float add_barrier_time = 15.0f;
 	float wait_time = 0.0f;
